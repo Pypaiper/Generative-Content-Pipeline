e2d8a5010162438eb930069a76b3018777138ba9
ryanmustari <ryan@viasocial.io>
2025-07-12 11:57:52 -0700

    initial user endpoints/setting up fastapi

---
+++ backend/src/models/user.py
@@ -0,0 +1,10 @@
+from models import Base
+from sqlalchemy.orm import Mapped, mapped_column
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    username: Mapped[str] = mapped_column(nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(nullable=False)
