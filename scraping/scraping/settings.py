cd04887a925d65760703d711b422880408d1e9b8
Alan Perry <40918666+ajperry2@users.noreply.github.com>
2025-07-18 22:02:17 -0700

    Dataset (#26)
    * initial backend and local infra setup
    
    * Fixing lint errors
    
    * bumping version
    
    * format check
    
    * draft project structure
    
    * Got data download working
    
    * Upping python version
    
    * Mono Repo Build Working
    
    * A working example of downloading data from the gutenberg project, saving to db/s3 and loading from them
    
    * fixing linting
    
    * updating linting to not look at notebooks
    
    * updating style in readme
    
    * draft of dataset class
    
    * fixxing lint issue
    
    * fixing formatting
    
    * linting fix
    
    ---------
    
    Co-authored-by: ryanmustari <ryan@viasocial.io>
    Co-authored-by: Alan Perry <ajperry2@usfca.edu>

--- scraping/scraping/settings.py
+++ scraping/scraping/settings.py
@@ -10,7 +10,7 @@ class _Settings(BaseSettings):
     DB_NAME: str = os.getenv("DB_NAME", "")
     DB_USER: str = os.getenv("DB_USER", "")
     DB_PORT: str = os.getenv("DB_PORT", "")
-    DB_PASSWORD: SecretStr = Field(os.getenv("DB_PASSWORD"))
+    DB_PASSWORD: SecretStr = Field(os.getenv("DB_PASSWORD", ""))
 
     # S3
     S3_HOST: str = os.getenv("S3_HOST", "")
@@ -19,7 +19,7 @@ class _Settings(BaseSettings):
     S3_REGION: str = os.getenv("S3_REGION", "")
     S3_BUCKET: str = os.getenv("S3_BUCKET", "")
 
-    SECRET_KEY: SecretStr = Field(os.getenv("SECRET_KEY"))
+    SECRET_KEY: SecretStr = Field(os.getenv("SECRET_KEY", ""))
 
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
