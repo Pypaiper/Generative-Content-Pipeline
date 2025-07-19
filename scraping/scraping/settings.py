63cb63707d6e56fb13ee256e1a405d8ce1d82c1a
Alan Perry <ajperry2@usfca.edu>
2025-07-18 21:59:11 -0700

    linting fix

--- scraping/scraping/settings.py
+++ scraping/scraping/settings.py
@@ -10,11 +10,7 @@ class _Settings(BaseSettings):
     DB_NAME: str = os.getenv("DB_NAME", "")
     DB_USER: str = os.getenv("DB_USER", "")
     DB_PORT: str = os.getenv("DB_PORT", "")
-<<<<<<< HEAD
     DB_PASSWORD: SecretStr = Field(os.getenv("DB_PASSWORD", ""))
-=======
-    DB_PASSWORD: SecretStr = Field(os.getenv("DB_PASSWORD"))
->>>>>>> master
 
     # S3
     S3_HOST: str = os.getenv("S3_HOST", "")
@@ -23,11 +19,7 @@ class _Settings(BaseSettings):
     S3_REGION: str = os.getenv("S3_REGION", "")
     S3_BUCKET: str = os.getenv("S3_BUCKET", "")
 
-<<<<<<< HEAD
     SECRET_KEY: SecretStr = Field(os.getenv("SECRET_KEY", ""))
-=======
-    SECRET_KEY: SecretStr = Field(os.getenv("SECRET_KEY"))
->>>>>>> master
 
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
