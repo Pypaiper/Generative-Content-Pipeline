accfa50384acfe1c7681d103baf7ce7d899107c8
Alan Perry <ajperry2@usfca.edu>
2025-07-18 21:44:57 -0700

    fixing formatting

--- training/training/dataset.py
+++ training/training/dataset.py
@@ -15,23 +15,20 @@ class CustomTextDataset(Dataset):
         self.engine = create_engine(connection_url(asyncronous=False))
         Session = sessionmaker(bind=self.engine)
         self.session = Session()
-
         self.length: int = self.session.query(Text).order_by(Text.id.desc()).first().id
-        
-            
+
     def __len__(self):
         return self.length
 
     def __getitem__(self, idx):
-
-        
-        ma = self.session.query(Text).where(Text.id == (idx+1) )
+        ma = self.session.query(Text).where(Text.id == (idx + 1))
         text = ma.first()
         image_path = text.image_path
         text = text.text
-        
-        download_file_from_s3(bucket_name=SETTINGS.S3_BUCKET, object_key=image_path, file=image_path)
+
+        download_file_from_s3(
+            bucket_name=SETTINGS.S3_BUCKET, object_key=image_path, file=image_path
+        )
         image = Image.open(image_path)
         os.remove(image_path)
         return text, image
-        
