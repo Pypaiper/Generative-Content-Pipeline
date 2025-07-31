b9b09518a18daf64b955109aa6437b4b46ff1c75
Alan Perry <40918666+ajperry2@users.noreply.github.com>
2025-07-31 00:29:16 -0700

    Ml utilities (#29)
    * initial backend and local infra setup
    
    * Fixing lint errors
    
    * bumping version
    
    * format check
    
    * draft project structure
    
    * Got data download working
    
    * Upping python version
    
    * Mono Repo Build Working
    
    * A working example of downloading data from the gutenberg project, saving to db/s3 and loading from them
    
    * fixing linting
    
    * updating linting to not look at notebooks
    
    * updating style in readme
    
    * draft of dataset class
    
    * fixxing lint issue
    
    * fixing formatting
    
    * linting fix
    
    * changing training project to a more intuitive ml name
    
    * moving tests into projects, copying opensora inference into file, and removing ability to dev ml on mac intel, it is just not possible even with platform specific dependencies, we require pytorch > 2.2
    
    * Shifting project name, working dev environment in base of repo needs some work
    
    * installing collosalai with pip
    
    * installing liger-kernel with pip
    
    * opencv-python-headless with pip
    
    * removing tensornvme from env
    
    * using pytorch for conda env
    
    * got development env working for opensora
    
    * reformatting imports so linting semipasses
    
    * updating package name
    
    * removing md formatting for now
    
    * trying to setup test env for opensora
    
    * removing test for now until we know what we want them to test
    
    * inference running in notebook with cpu need to optimize for development
    
    * removing pycache
    
    * fix setup port in readme
    
    * adding local secrets to dev env
    
    * adding aws localstack resources back into dev env
    
    * adding dev dependencies to project for jupyter support
    
    * pyproject changes for dev env
    
    * adding gutenburgpy to dev env
    
    * adding requests to scraping dev env
    
    * scraping operational in notebook
    
    * ignore cache from data scraping
    
    * fixing linting
    
    * adding all services to docker-compose file
    
    * simplifying Dockerfile
    
    * adding workspace to login link in readme
    
    * adding basic terraform to test success
    
    * linting fix
    
    ---------
    
    Co-authored-by: ryanmustari <ryan@viasocial.io>
    Co-authored-by: Alan Perry <ajperry2@usfca.edu>
    Co-authored-by: Alan <quadrangle43@gmail.com>

---
+++ infra/remote/main.tf
@@ -0,0 +1,11 @@
+
+
+provider "aws" {
+  region = var.region # Example for AWS, replace with your desired region
+}
+
+
+resource "aws_instance" "example_instance" {
+  ami           = "ami-054b7fc3c333ac6d2" # Replace with a valid AMI ID for your region
+  instance_type = "t2.micro"
+}
