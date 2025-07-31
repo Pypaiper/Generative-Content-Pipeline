1763fcbc16a0d0776acf506401ab7224338d46d8
Alan <quadrangle43@gmail.com>
2025-07-31 00:31:46 -0700

    testing change to infra

--- infra/remote/variables.tf
+++ infra/remote/variables.tf
@@ -1,5 +1,5 @@
 variable "region" {
-    description = "The region of deployment."
+    description = "The region of deployment"
     type        = string
     sensitive   = true # Mark as sensitive to prevent logging
 }
