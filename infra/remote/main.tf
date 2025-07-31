2b8bd4a7422ead6e7c0bd1723c8b44028f85d1af
Alan <quadrangle43@gmail.com>
2025-07-31 02:15:26 -0700

    setting var file

--- infra/remote/main.tf
+++ infra/remote/main.tf
@@ -12,11 +12,13 @@ provider "aws" {
   region = var.region # Example for AWS, replace with your desired region
 }
 
-resource "aws_s3_bucket" "book" {
-  bucket = "book-terraform-s3-bucket-name" # Choose a globally unique bucket name
-  acl    = "private" # Or "public-read", "public-read-write", etc., depending on your needs
-  tags = {
-    Environment = "Deployment"
-    Project     = "Generative-Content-Pipeline"
-  }
+
+data "aws_s3_bucket" "book" {
+  bucket = "book-terraform-s3-bucket-name"
 }
+
+output "my_bucket_name" {
+  value = "${data.aws_s3_bucket.mybucket.bucket}"
+}
+
+
