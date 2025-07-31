19b57d1daf0eb058ac6c544f89a5b9eb381d9422
Alan <quadrangle43@gmail.com>
2025-07-31 01:53:52 -0700

    setting var file

--- infra/remote/main.tf
+++ infra/remote/main.tf
@@ -11,7 +11,7 @@ provider "aws" {
 }
 
 
-resource "aws_instance" "example_instance" {
-  ami           = "ami-054b7fc3c333ac6d2" # Replace with a valid AMI ID for your region
-  instance_type = "t2.micro"
-}
+# resource "aws_instance" "example_instance" {
+#   ami           = "ami-054b7fc3c333ac6d2" # Replace with a valid AMI ID for your region
+#   instance_type = "t2.micro"
+# }
