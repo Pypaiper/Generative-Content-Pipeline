ae4713138c77e39ef0d38ab24a303f5724dea70d
Alan <quadrangle43@gmail.com>
2025-07-31 02:09:49 -0700

    setting var file

--- infra/remote/main.tf
+++ infra/remote/main.tf
@@ -6,21 +6,14 @@ variable "region" {
     sensitive   = true # Mark as sensitive to prevent logging
 }
 
-variable "bucket_name" {
-
-    description = "The region of deployment."
-    type        = string
-    sensitive   = true # Mark as sensitive to prevent logging
-}
-
 
 
 provider "aws" {
   region = var.region # Example for AWS, replace with your desired region
 }
 
-resource "aws_s3_bucket" var.bucket_name {
-  bucket = "my-unique-terraform-s3-bucket-name" # Choose a globally unique bucket name
+resource "aws_s3_bucket" "book" {
+  bucket = "book-terraform-s3-bucket-name" # Choose a globally unique bucket name
   acl    = "private" # Or "public-read", "public-read-write", etc., depending on your needs
   tags = {
     Environment = "Deployment"
