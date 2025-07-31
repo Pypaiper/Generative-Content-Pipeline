bac19290dcda23f403c5ab13161edb8dc65dcad1
Alan <quadrangle43@gmail.com>
2025-07-31 01:44:18 -0700

    setting var file

--- infra/remote/main.tf
+++ infra/remote/main.tf
@@ -1,4 +1,10 @@
 
+variable "region" {
+
+    description = "The region of deployment."
+    type        = string
+    sensitive   = true # Mark as sensitive to prevent logging
+}
 
 provider "aws" {
   region = var.region # Example for AWS, replace with your desired region
