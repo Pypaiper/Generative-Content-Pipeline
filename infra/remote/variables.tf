fdf5a25c75797ca995885968558c6a1cd514e86d
Alan <quadrangle43@gmail.com>
2025-07-31 00:59:16 -0700

    terraform needs it's plan

--- infra/remote/variables.tf
+++ infra/remote/variables.tf
@@ -1,11 +1,6 @@
 variable "region" {
 
-<<<<<<< HEAD
     description = "The region of deployment"
-=======
-
-    description = "The region of deployment."
->>>>>>> 7e7c9ba1a2e15dd76c9b22821a0b217b0f07b90a
     type        = string
     sensitive   = true # Mark as sensitive to prevent logging
 }
