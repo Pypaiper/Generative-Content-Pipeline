1f7e6c6fa7a4e085d09f316e2dbeaf759e6729b0
Alan <quadrangle43@gmail.com>
2025-08-04 17:46:07 -0700

    fixing variable name in action

--- infra/remote/main.tf
+++ infra/remote/main.tf
@@ -130,7 +130,7 @@ resource "aws_db_instance" "rds_instance" {
   engine_version       = "8.0"
   instance_class       = "db.t3.micro"
   db_name              = var.db_name
-  username             = var.db_username
+  username             = var.db_user
   password             = var.db_password # Use secrets management in production
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
